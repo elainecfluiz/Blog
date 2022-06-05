e6df10d348101c6c17bf9b8719179928c712ae82
Carlos Timoteo Arruda <ctaecfl00001@gmail.com>
2022-06-05 13:00:01 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const setaRight = document.getElementById("setaRight");
+const setaLeft = document.getElementById("setaLeft");
+
+const artigos = document.getElementsByClassName("artigo")
+
+let flag_Artigo = 0;
+
+function exibirArtigos(){
+    if(flag_Artigo < (artigos.length)){
+        for(i = 0; i <= (artigos.length -1); i++){
+            artigos[i].style.display= "none";
+        };
+        artigos[flag_Artigo].style.display="block";
+    }
+    
+
+}
+
+function incrementando_Flag_Artigo(){
+    if(flag_Artigo <= (artigos.length - 1)){
+        flag_Artigo++;
+    };
+};
+
+function decrementando_Flag_Artigo(){
+    if(flag_Artigo > 0){
+        flag_Artigo--;
+    }
+};
+
+setaRight.addEventListener("click", function (){
+    incrementando_Flag_Artigo();
+    exibirArtigos()
+});
+
+setaLeft.addEventListener("click", function (){
+    decrementando_Flag_Artigo();
+    exibirArtigos()
+});
+
+exibirArtigos();
+decrementando_Flag_Artigo();
